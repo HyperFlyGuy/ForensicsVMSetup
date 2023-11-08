e86a0185ed576cefd87b53c5c8107d6b24eeb2a0
HyperFlyGuy <connor.spangler@protonmail.com>
2023-11-08 09:25:42 -0600

    V1 Complete

--- install.ps1
+++ install.ps1
@@ -155,6 +155,7 @@ function Install-OtherPrograms {
          }
          refresh-path
     }
+    .\Get-ZimmermanTools.ps1 -Dest 'C:\Program Files\Forensic Tools\Analysis'
 }
 function New-Shortcuts {
     param (
@@ -177,6 +178,67 @@ function New-Shortcuts {
     $Shortcut.TargetPath = "C:\Program Files\Forensic Tools"
     $Shortcut.Save()
 }
+function Set-DesktopWallpaper {
+    <#
+    .DESCRIPTION
+        Sets a desktop background image
+    .PARAMETER PicturePath
+        Defines the path to the picture to use for background
+    .PARAMETER Style
+        Defines the style of the wallpaper. Valid values are, Tiled, Centered, Stretched, Fill, Fit, Span
+    .EXAMPLE
+        Set-DesktopWallpaper -PicturePath "C:\pictures\picture1.jpg" -Style Fill
+    .EXAMPLE
+        Set-DesktopWallpaper -PicturePath "C:\pictures\picture2.png" -Style Centered
+    .NOTES
+        Supports jpg, png and bmp files.
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][String]$PicturePath,
+        [ValidateSet('Tiled', 'Centered', 'Stretched', 'Fill', 'Fit', 'Span')]$Style = 'Fill'
+    )
+
+
+    BEGIN {
+        $Definition = @"
+[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+"@
+
+        Add-Type -MemberDefinition $Definition -Name Win32SystemParametersInfo -Namespace Win32Functions
+        $Action_SetDeskWallpaper = [int]20
+        $Action_UpdateIniFile = [int]0x01
+        $Action_SendWinIniChangeEvent = [int]0x02
+
+        $HT_WallPaperStyle = @{
+            'Tiles'     = 0
+            'Centered'  = 0
+            'Stretched' = 2
+            'Fill'      = 10
+            'Fit'       = 6
+            'Span'      = 22
+        }
+
+        $HT_TileWallPaper = @{
+            'Tiles'     = 1
+            'Centered'  = 0
+            'Stretched' = 0
+            'Fill'      = 0
+            'Fit'       = 0
+            'Span'      = 0
+        }
+
+    }
+
+
+    PROCESS {
+        Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name wallpaperstyle -Value $HT_WallPaperStyle[$Style]
+        Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name tilewallpaper -Value $HT_TileWallPaper[$Style]
+        $null = [Win32Functions.Win32SystemParametersInfo]::SystemParametersInfo($Action_SetDeskWallpaper, 0, $PicturePath, ($Action_UpdateIniFile -bor $Action_SendWinIniChangeEvent))
+    }
+}
 function Main {
     $Index=Import-Csv -path .\ExtraToolsIndex.txt
     Show-System
@@ -184,5 +246,6 @@ function Main {
     New-FileStructure
     Install-OtherPrograms $Index $cred
     New-Shortcuts $Index
+    Set-DesktopWallpaper -PicturePath $PSScriptRoot\Resources\wallpaper.png -Style Fit
 }
 Main
